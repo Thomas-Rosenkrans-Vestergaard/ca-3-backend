ef196d10ef7e9e7696c36219be6845cf9d526b1a
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-13 15:08:12 +0100

    fixed import

--- src/main/java/com/group3/ca3/logic/authentication/AuthenticationFacade.java
+++ src/main/java/com/group3/ca3/logic/authentication/AuthenticationFacade.java
@@ -1,13 +1,13 @@
 package com.group3.ca3.logic.authentication;
 
 import com.group3.ca3.data.entities.User;
-import com.group3.ca3.logic.passwordencryption.PasswordEncryption;
 import com.group3.ca3.logic.jwt.AuthenticationContext;
 import com.group3.ca3.repositories.JpaUserRepository;
 import com.group3.ca3.rest.JpaConnection;
 import org.mindrot.jbcrypt.BCrypt;
 
-public class AuthenticationFacade {
+public class AuthenticationFacade
+{
 
 
     private String timingHash = "$2a$10$TcspcbNbrPac2XnfEBdgVeUfhxkUDZ9hvls5jcRAqb6EODbkAKnUy";
@@ -15,7 +15,8 @@ public class AuthenticationFacade {
     private JpaUserRepository jpaRepository = new JpaUserRepository(JpaConnection.create());
 
 
-    public AuthenticationContext authenticateUser(String email, String password) {
+    public AuthenticationContext authenticateUser(String email, String password)
+    {
         User user = jpaRepository.getByEmail(email);
 
         boolean checkHash = checkPassword(password, user == null ? timingHash : user.getPasswordHash());
@@ -28,7 +29,8 @@ public class AuthenticationFacade {
     }
 
 
-    private Boolean checkPassword(String password, String hashedPassword) {
+    private Boolean checkPassword(String password, String hashedPassword)
+    {
         return BCrypt.checkpw(password, hashedPassword);
     }
 }
